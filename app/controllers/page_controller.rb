5870b566c6d78c6a6615217d6e96366679c7b848
NorthZebra <privatzeus@gmail.com>
2016-09-28 16:20:42 +0300

    some ganges

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -2,7 +2,5 @@ class PageController < ApplicationController
   
   def index
     @news = News.ordered_by_date.paginate(:page => params[:page], :per_page => 10)
-
-    #@news_bar = News.includes(:comments).order("created_at desc").limit(1)
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :news
+
+  scope :comment_by_date, -> { order created_at: :desc }
 end
